a79e7fc3c808f9159d754376fd21e13c34b238aa
Boga <bogac@science.regn.net>
2026-08-10 07:55:36 +0100

    message

--- Todo project/Todo.js
+++ Todo project/Todo.js
@@ -1,11 +1,32 @@
  const todoarray = [];
-    function addTodo(){
+
+ 
+    
+ 
+ function displayTodos(){
+
+  let todoElements = "";
+
+ for(let i=0;i<todoarray.length;i++){
+  const item = todoarray[i];
+  const html = `<p>${item}</p>`
+
+  todoElements =  todoElements + html;
+ }
+ document.querySelector('.js-todos').innerHTML = todoElements;
+
+ }
+ 
+ 
+ 
+ function addTodo(){
     
      const inputElement = document.querySelector('.js-input');
      const inputValue = inputElement.value;
      console.log(inputValue);
      todoarray.push(inputValue);
      console.log(todoarray);
+     displayTodos();
      inputElement.value = "";
     
       
