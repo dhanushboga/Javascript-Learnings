76a75b755cdb55a1d2db2e9ce36046034a9988d5
Boga <bogac@science.regn.net>
2026-08-10 08:30:17 +0100

    message

---
+++ Todo project(improved_version))/Todo.js
@@ -0,0 +1,56 @@
+ const todoarray = [
+  {
+    name:"html",
+    dueDate:"2022-12-22",
+  },
+
+  {
+    name: "css",
+    dueDate: "2022-12-22",
+  }
+
+  ];
+
+ function displayTodos(){
+
+  let todoElements = "";
+
+ for(let i=0;i<todoarray.length;i++){
+  const item = todoarray[i];
+  //const name = itemObject.name;
+ // const dueDate =  itemObject.dueDate;
+  const {name,dueDate} = itemObject;
+  const html = `<p>
+               ${name} ${dueDate}
+               <button onclick="
+                 todoarray.splice(${i},1);
+                 displayTodos();
+               ">Delete</button> 
+               </p>`;
+
+  todoElements =  todoElements + html;
+ }
+ document.querySelector('.js-todos').innerHTML = todoElements;
+
+ }
+ 
+ 
+ 
+ function addTodo(){
+    
+     const inputElement = document.querySelector('.js-input');
+     const dateElement = document.querySelector('.js-date');
+     
+     const inputValue = inputElement.value;
+     const dateValue = dateElement.value;
+     
+     console.log(inputValue);
+     todoarray.push({name:inputValue,dueDate});
+
+     console.log(todoarray);
+     displayTodos();
+     inputElement.value = "";
+    
+      
+   }
+    
